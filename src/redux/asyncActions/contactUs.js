3daf6e191ceba0b3f1e3b70f93ccb0a3e04c42ef
prasetyoow <praztamer@gmail.com>
2022-09-30 16:53:43 +0700

    change database url

--- src/redux/asyncActions/contactUs.js
+++ src/redux/asyncActions/contactUs.js
@@ -1,11 +1,11 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+import http from "../../helpers/http"
 
 export const postData = createAsyncThunk('contact-us/createData', async(request) => {
   try {
     const send = new URLSearchParams(request).toString()
     console.log(send)
-    const {data} = await axios.post('http://localhost:8080/contact-us')
+    const {data} = await http().post('contact-us')
     return data
   }catch (e) {
     console.log(e)
@@ -17,7 +17,7 @@ export const getStoredData = createAsyncThunk('contact-us/getAllData', async({li
   page = parseInt(page) || 1
   try {
     const query = new URLSearchParams({limit, page, keyword, sortType}).toString()
-    const {data} = await axios.get('http://localhost:8080/contact-us?'+query)
+    const {data} = await http().get('contact-us?'+query)
     return data
   } catch (e) {
     console.log(e)
