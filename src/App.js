7b05a61fd80bcb34c2242533477b1673ede8a1aa
prasetyoow <praztamer@gmail.com>
2022-09-30 16:00:16 +0700

    fixed routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import {Routes, Route} from 'react-router-dom';
+
 import FormContactUs from "./pages/formContactUs";
 import GetContactUs from "./pages/getContactUs";
 
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<FormContactUs />} />
-      <Route path="/getData" element={<GetContactUs />} />
-    </Routes>
+        <Routes>
+          
+            <Route path="/" element={<FormContactUs />} />
+            <Route path="/contactData" element={<GetContactUs />} />
+        </Routes>
   )
 }
 
